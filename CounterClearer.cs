54a8899ddcb54841c041894e2a3fffaa1e201339
Julie Lerman <julielerman@gmail.com>
2019-10-03 15:33:16 -0400

    working on readable output

---
+++ CounterClearer.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
+namespace DataPoints.Function
+{
+    public static class CounterClearer
+    {
+        [FunctionName("CounterClearer")]
+        public static async Task<int> RunOrchestartor(
+          [OrchestrationTrigger] IDurableOrchestrationContext context)
+        {
+            var entityId = new EntityId(nameof(Counter), "key");
+            context.SignalEntity(entityId, "Reset");
+            return await context.CallEntityAsync<int>(entityId, "Get");
+        }
+        [FunctionName("HttpTriggerClear")]
+        public static async Task<IActionResult> Http(
+          [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req,
+          [DurableClient] IDurableClient client)
+        {
+            string id = await client.StartNewAsync("CounterClearer", null);
+            return client.CreateCheckStatusResponse(req, id);
+        }
+    }
+
+}
--- CounterOrchestration.cs
+++ CounterOrchestration.cs
@@ -8,60 +8,35 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
-using Newtonsoft.Json.Serialization;
 
-namespace DataPoints.Function {
-    public static class CounterOrchestration {
-      [FunctionName ("CounterOrchestration")]
-      public static async Task<string> RunOrchestartor (
-        [OrchestrationTrigger] IDurableOrchestrationContext context) {
-        var entityId = new EntityId (nameof (Entity), "key");
-       context.SignalEntity(entityId, "Add", 2);
-        // Synchronous call to the entity which returns a value
-        var currentValue = await context.CallEntityAsync<int>(entityId, "Add",1);
-        var x=3;
-        return "xyz";
-        // if (currentValue < 10) {
-        //   // Asynchronous call which updates the value
-        //   context.SignalEntity(entityId, "Add", 1);
-        // }
-      }
-    
-      public class Entity {
-
-        [FunctionName ("Entity")]
-        public static void Counter ([EntityTrigger] IDurableEntityContext ctx)
+namespace DataPoints.Function
+{
+    public static class CounterOrchestration
+    {
+        [FunctionName("CounterOrchestration")]
+        public static async Task<int> RunOrchestartor(
+          [OrchestrationTrigger] IDurableOrchestrationContext context)
+        {
+            var entityId = new EntityId(nameof(Counter), "key");
+            var currentValue = await context.CallEntityAsync<int>(entityId, "Get");
+            if (currentValue < 10)
             {
-                int initState = System.Convert.ToInt32(((JValue)((JProperty)(ctx.GetState<JObject>()).First).Value).Value);
+                // Asynchronous call which updates the value
+                context.SignalEntity(entityId, "Add", 1);
+            }
+            var finalValue= await context.CallEntityAsync<int>(entityId, "Get");
+           return finalValue;
+           
 
-                ctx.SetState(initState);
-                // var y = ctx.GetState<int>();
-            
-                      var currentValue = ctx.GetState<int>();
-              
-                 switch (ctx.OperationName.ToLowerInvariant ()) {
-            case "add":
-              int amount = ctx.GetInput<int> ();
-              currentValue += amount;
-              break;
-            case "reset":
-              currentValue = 0;
-              break;
-            case "get":
-              ctx.Return (currentValue);
-              break;
-          }
-
-          ctx.SetState (currentValue);
         }
-      }
-       [FunctionName("HttpTrigger")]
+        [FunctionName("HttpTrigger")]
         public static async Task<IActionResult> Http(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req,
-            [DurableClient] IDurableClient client)
+          [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req,
+          [DurableClient] IDurableClient client)
         {
             string id = await client.StartNewAsync("CounterOrchestration", null);
             return client.CreateCheckStatusResponse(req, id);
         }
     }
+
 }
